27ead26d466a638167c252d7833478b47233a92c
autumnfall <452264063@qq.com>
2017-01-10 03:12:35 +0000

    finish layout and routes

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,10 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
     <title>@yield('title', 'Sample')- Laravel 入门教程</title>
+    <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
-    @yield('content')
+    @include('layouts._header')
+    <div class="container">
+      @yield('content')
+      @include('layouts._footer')
+    </div>
 </body>
 </html>
